785cd465ca59d4ea6b7ad2bf533eb4d19b91bb5c
Jess <code@jessinthe.cloud>
2021-05-15 23:21:47 -0400

    add home text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,10 @@
 @endsection
 
 @section('content')
+
+<h1>Welcome!</h1>
+
+<p>If your memory is as terrible as mine, 
+feel free to use the search or recipe menu to find what you need </p>
     
 @endsection
