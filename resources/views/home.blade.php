10deb743b5345ec5b028cca237d3fce7ffdc8032
Jess <code@jessinthe.cloud>
2021-05-16 17:31:36 -0400

    add github link, make homepage nicer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,16 @@
 
 @section('content')
 
-<h1 class="w-full text-4xl mb-4">Welcome!</h1>
+<h1 class="w-full text-4xl mb-4">Welcome to Valheim Recipes</h1>
 
-<p>
-    If your memory is as terrible as mine, 
-    feel free to use the search or recipe menu to find the Valheim recipe that you need 
+<p class="my-2">
+    We're under construction, but you can still use the search box or menu to find the Valheim recipe that you need.
+</p>
+<p class="my-2">
+    This site uses the item and recipes data extracted directly from the game, thanks to a modified version of JotunnDoc from <a rel="noopener nofollow" href="https://github.com/Valheim-Modding/Jotunn">Jötunn, the Valheim Library</a> that I created using their excellent mod tools and tutorials. 
+</p>
+<p class="my-2">
+    If you find any undocumented problems or have a request, feel free to <a class="underline hover:no-underline" rel="noopener nofollow" href="https://github.com/jessinthecloud/valheim/issues/new/choose">open an issue</a> on github
 </p>
 
 <section class="w-full flex flex-wrap mt-8">
