e30e5abc37f739b29c7dc08c81aff195028c4042
Jess <code@jessinthe.cloud>
2021-05-16 14:33:23 -0400

    Fixes #64 & cleans up recipe controller & views

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -14,42 +14,11 @@ class RecipeController extends Controller
      */
     public function index()
     {
-        $totals = [];
-        $totals_strings = [];
-        $upgrades = [];
-        $recipes = Recipe::with([
-            'requirements' => function ($query) {
-                $query->orderByDesc('amount', SORT_NUMERIC)->orderByDesc('name', SORT_NATURAL|SORT_FLAG_CASE);
-            },
-        ])->orderBy('name', 'asc')->get()->map(function ($recipe) use (&$totals, &$upgrades, &$totals_strings) {
-            $recipe->name = ucwords($recipe->name);
-            $max_quality = $recipe->item->sharedData->max_quality ?? 1;
-            $recipe->max_quality = $max_quality;
+        $recipes = Recipe::with($this->relationsSubQuery())->orderBy('name', 'asc')->get()->map(function ($recipe) {
+            return $recipe = $this->formatForView($recipe);
+        });
 
-            // start at the first upgrade level and determine the required item amounts
-            for ($i=2; $i<=$max_quality; $i++) {
-                foreach ($recipe->requirements as $req) {
-                    $upgrades[$i][$req->name]= $req->getAmount($i);
-                    $sum = array_sum(array_column($upgrades, $req->name));
-                    // dump("req:", $req, "totals: ", $totals);
-                    $totals [$req->name]= '<strong>'.($sum+$req->amount).'</strong>';
-                } // end foreach
-            } // end for
-
-            $totals_strings []= urldecode(str_replace('=', ' ', http_build_query(array_flip($totals), null, ', ')));
-
-            // dump($recipe);
-            // dump("max level: $max_quality");
-            // dump($upgrades);
-            // dump($totals);
-            return $recipe;
-        })->filter();
-
-        // TODO take care of this for multiple recipes returned
-        $totals = '';
-        $upgrades = [];
-
-        return view('recipes.index', compact('recipes', 'upgrades', 'totals'));
+        return view('recipes.index', compact('recipes'));
     }
 
     /**
@@ -74,85 +43,94 @@ class RecipeController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified resource (by id)
      *
      * @param  \App\Models\Recipe  $recipe
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        $recipe = Recipe::with([
-            'requirements' => function ($query) {
-                $query->orderByDesc('amount', SORT_NUMERIC)->orderByDesc('name', SORT_NATURAL|SORT_FLAG_CASE);
-            },
-        ])->findOrFail($id);
+        $recipe = Recipe::with($this->relationsSubQuery())->findOrFail($id);
 
-        $recipe->name = ucwords($recipe->name);
-        $max_quality = $recipe->item->sharedData->max_quality;
-        $recipe->max_quality = $max_quality;
-        $totals = [];
-        $upgrades = [];
-        // start at the first upgrade level and determine the required item amounts
-        for ($i=2; $i<=$max_quality; $i++) {
-            foreach ($recipe->requirements as $req) {
-                $upgrades[$i][$req->name]= $req->getAmount($i);
-                $sum = array_sum(array_column($upgrades, $req->name));
+        $recipe = $this->formatUpgradesForView($this->formatForView($recipe));
 
-                $totals [$req->name]= '<strong>'.($sum+$req->amount).'</strong>';
-            } // end foreach
-        } // end for
-
-        $totals = urldecode(str_replace('=', ' ', http_build_query(array_flip($totals), null, ', ')));
-        /*
-                dump($recipe);
-                dump("max level: ".$recipe->item->sharedData->max_quality);
-                dump($upgrades);
-                dump($totals);*/
-
-        return view('recipes.show', compact('recipe', 'upgrades', 'totals'));
+        return view('recipes.show', compact('recipe'));
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified resource (by slug)
      *
      * @param  \App\Models\r  $r
      * @return \Illuminate\Http\Response
      */
     public function showSlug($slug)
     {
-        $recipe = Recipe::with([
+        $recipe = Recipe::with($this->relationsSubQuery())->where('slug', $slug)->firstOrFail();
+
+        $recipe = $this->formatUpgradesForView($this->formatForView($recipe));
+
+        return view('recipes.show', compact('recipe'));
+    }
+
+    protected function relationsSubQuery()
+    {
+        return [
             'requirements' => function ($query) {
                 $query->orderByDesc('amount', SORT_NUMERIC)->orderByDesc('name', SORT_NATURAL|SORT_FLAG_CASE);
             },
-        ])->where('slug', $slug)->firstOrFail();
+        ];
+    }
 
+    protected function formatForView($recipe)
+    {
         $recipe->name = ucwords($recipe->name);
-        $max_quality = $recipe->item->sharedData->max_quality;
-        $recipe->max_quality = $max_quality;
-        $totals = [];
+        $recipe->max_quality = $recipe->item->sharedData->max_quality ?? 1;
+
+        return $recipe;
+    }
+
+    protected function formatUpgradesForView($recipe)
+    {
+        $recipe->name = ucwords($recipe->name);
+        $recipe->max_quality = $recipe->item->sharedData->max_quality ?? 1;
         $upgrades = [];
+        $sum = [];
         // start at the first upgrade level and determine the required item amounts
-        for ($i=2; $i<=$max_quality; $i++) {
+        for ($i=2; $i<=$recipe->max_quality; $i++) {
             $upgrades[$i]= [
                 'station' => $recipe->getRequiredStation($i)->name,
                 'station_level' => $recipe->getRequiredStationLevel($i),
             ];
             foreach ($recipe->requirements as $req) {
                 $upgrades[$i]['resources'][$req->name]= $req->getAmount($i);
-                $sum = array_sum(array_column($upgrades, $req->name));
-
-                $totals [$req->name]= '<strong>'.($sum+$req->amount).'</strong>';
+                if (isset($sum[$req->name])) {
+                    $sum[$req->name] += $upgrades[$i]['resources'][$req->name];
+                } else {
+                    $sum[$req->name] = $upgrades[$i]['resources'][$req->name]+$req->amount;
+                }
             } // end foreach
         } // end for
-
-        $totals = urldecode(str_replace('=', ' ', http_build_query(array_flip($totals), null, ', ')));
 
         // dump($recipe);
         // dump("max level: ".$recipe->item->sharedData->max_quality);
         // dump($upgrades);
-        // dump($totals);
+        // dump($sum);
 
-        return view('recipes.show', compact('recipe', 'upgrades', 'totals'));
+        $totals = '';
+        foreach ($sum as $item => $amount):
+            if ($amount > 0) {
+                $totals .= '<strong>'.$amount.'</strong> '.$item.', ';
+            }
+        endforeach;
+        $totals = rtrim($totals, ', ');
+
+        $recipe->upgrades = $upgrades;
+        $recipe->totals = $totals;
+
+        unset($upgrades);
+        unset($totals);
+
+        return $recipe;
     }
 
     /**
--- resources/views/partials/_recipe.blade.php
+++ resources/views/partials/_recipe.blade.php
@@ -1,5 +1,7 @@
 <section class="max-w-full flex-grow bg-gray-800 mb-4 mr-4 p-6 md:w-5/12">
-    <h2 class="w-full text-3xl mb-4">{{ $recipe->name }} Recipe</h2>
+    <h2 class="w-full text-3xl mb-4">
+        {{ $recipe->name }} Recipe <span class="float-right text-xs">{{ $recipe->id }}</span>
+    </h2>
     @if($recipe->item)
         <p class="my-4">
             {{ $recipe->item->sharedData->description }}
@@ -33,40 +35,9 @@
             @endif
         @endforeach
     </ul>
-    @if($recipe->max_quality > 1)
-        <h3 class="w-full text-xl mt-4">Upgrades:</h3>
-        <div class="bg-gray-900 mt-4 p-4">
-            @if(!empty($upgrades))
-            <ul class="">
-                @foreach($upgrades as $level => $upgrade)
-                    <li class="bg-gray-800 mb-4 p-4">
-                        <h4 class="text-lg font-bold mb-2">
-                            Level {{ $level }}
-                            @if($level === $recipe->max_quality)
-                                (MAX)
-                            @endif
-                        </h4>
-                        <ul class="pl-4">
-                            <li>
-                                @if(isset($upgrade['station']) && $upgrade['station_level'] > 1) 
-                                    <em class="block mb-2">{{ $upgrade['station'] }} Level {{ $upgrade['station_level'] }}</em>  
-                                @endif
-                            </li>
-                        @foreach($upgrade['resources'] as $item => $amount)
-                            <li>
-                                @if($amount > 0)
-                                    <strong>{{ $amount }}</strong> {{ $item }}
-                                @endif
-                            </li>
-                        @endforeach
-                        </ul>
-                    </li>
-                @endforeach
-            </ul>
-            <p class="mt-4">
-                <em>Total Resources Needed: {!! $totals !!}</em>
-            </p>
-            @endif
-        </div>
+    @if($recipe->max_quality > 1 && !empty($recipe->upgrades))
+        @include('partials._recipe_upgrades', [
+            'recipe'    => $recipe
+        ])
     @endif
 </section>
