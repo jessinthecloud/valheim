33d4db3893e0ea223a7319ca5a358d80ea80a440
Jess <code@jessinthe.cloud>
2021-09-27 13:23:48 -0400

    recipe upgrade list styles

--- resources/views/partials/_recipe.blade.php
+++ resources/views/partials/_recipe.blade.php
@@ -1,56 +1,50 @@
-<section class="max-w-full flex-grow bg-gray-800 mb-4 mr-4 p-6 md:w-5/12">
+<section class="max-w-full flex-grow flex flex-wrap justify-between bg-gray-800 mb-4 mr-4 p-6 md:w-5/12">
     <h2 class="relative w-full text-3xl mb-4">
         {{ $recipe->name }} <span class="absolute top-0 right-0 text-gray-500 text-xs">{{ $recipe->id }}</span>
     </h2>
-    @if($recipe->item)
-        <p class="my-4">
-            {!! $recipe->item->sharedData->description !!}
-        </p>
-        @if($recipe->craftingStation)
-            <p class="mt-4">
-                <strong>Crafting station:</strong> 
-                <em>
-                    {{ $recipe->craftingStation->name }} 
-                    @if($recipe->min_station_level > 1)
-                        Level {{ $recipe->min_station_level }}
-                    @endif
-                </em>
+    <div class="recipe-details w-full md:mr-6 md:w-1/2">
+        @if($recipe->item)
+            <p class="my-4">
+                {!! $recipe->item->sharedData->description !!}
             </p>
-        @endif
-        @if($recipe->amount > 1)
-            <p class="">
-                <strong>Creates:</strong> <em>{{ $recipe->amount }}</em>
-            </p>
-        @endif
-    @endif
-    <p class="my-2">
-        <strong>Resources:</strong>
-    </p>
-    <ul class="pl-4">
-        @foreach($recipe->requirements as $requirement)
-            @if($requirement->amount > 0)
-                <li>
-                    <strong>{{ $requirement->amount }}</strong> 
-                    <a 
-                    class="underline hover:text-amber hover:no-underline transition ease-in-out duration-150" 
-                    href="{{ route('items.show', $requirement->item->slug) }}">{{ $requirement->name }}</a>
-                </li>
+            @if($recipe->craftingStation)
+                <p class="mt-4">
+                    <strong>Crafting station:</strong> 
+                    <em>
+                        {{ $recipe->craftingStation->name }} 
+                        @if($recipe->min_station_level > 1)
+                            Level {{ $recipe->min_station_level }}
+                        @endif
+                    </em>
+                </p>
             @endif
-        @endforeach
-    </ul>
-    @if(isset($is_listing) && $recipe->max_quality > 1)
-        <p class="mt-4">
-            <a 
-                class="inline-block my-1 py-3 px-6 bg-amber-900 font-semibold hover:bg-amber-200 hover:text-black transition ease-in-out duration-150" 
-                href="{{ route('recipes.show', $recipe->slug) }}"
-            >
-                View Details
-            </a>
+            @if($recipe->amount > 1)
+                <p class="">
+                    <strong>Creates:</strong> <em>{{ $recipe->amount }}</em>
+                </p>
+            @endif
+        @endif
+        <p class="my-2">
+            <strong>Resources:</strong>
         </p>
-    @endif 
+        <ul class="pl-4">
+            @foreach($recipe->requirements as $requirement)
+                @if($requirement->amount > 0)
+                    <li>
+                        <strong>{{ $requirement->amount }}</strong> 
+                        <a 
+                        class="underline hover:text-amber hover:no-underline transition ease-in-out duration-150" 
+                        href="{{ route('items.show', $requirement->item->slug) }}">{{ $requirement->name }}</a>
+                    </li>
+                @endif
+            @endforeach
+        </ul>
+    </div>
+    
     @if($recipe->max_quality > 1 && !empty($recipe->upgrades))
         @include('partials._recipe_upgrades', [
             'recipe'    => $recipe
         ])
     @endif
+    
 </section>
--- resources/views/partials/_recipe_upgrades.blade.php
+++ resources/views/partials/_recipe_upgrades.blade.php
@@ -1,35 +1,37 @@
-<h3 class="w-full text-xl mt-4">Upgrades:</h3>
-<div class="bg-gray-900 mt-4 p-4">
-    <ul class="">
-        @foreach($recipe->upgrades as $level => $upgrade)
-            <li class="bg-gray-800 mb-4 p-4">
-                <h4 class="text-lg font-bold mb-2">
-                    Level {{ $level }}
-                    @if($level === $recipe->max_quality)
-                        (MAX)
-                    @endif
-                </h4>
-                <ul class="pl-4">
-                    <li>
-                        @if(isset($upgrade['station']) && $upgrade['station_level'] > 1) 
-                            <em class="block mb-2">Level {{ $upgrade['station_level'] }} {{ $upgrade['station'] }} </em>  
+<section class="upgrades-wrapper w-full md:w-1/2 lg:w-1/3">
+    <div class="bg-gray-900 mt-4 p-4">
+        <h3 class="w-full text-xl mb-4">Upgrades</h3>
+        <ul class="w-full">
+            @foreach($recipe->upgrades as $level => $upgrade)
+                <li class="bg-gray-800 mb-4 p-4">
+                    <h4 class="text-lg font-bold mb-2">
+                        Level {{ $level }}
+                        @if($level === $recipe->max_quality)
+                            (MAX)
                         @endif
-                    </li>
-                @foreach($upgrade['resources'] as $item => $amount)
-                    <li>
-                        @if($amount > 0)
-                            <strong>{{ $amount }}</strong> {{ $item }}
-                        @endif
-                    </li>
-                @endforeach
-                </ul>
-            </li>
-        @endforeach
-    </ul>
-    <p class="mt-4">
-        <em>
-            Total resources to max level: 
-            {!! $recipe->totals !!}
-        </em>
-    </p>
-</div>
+                    </h4>
+                    <ul class="pl-4">
+                        <li>
+                            @if(isset($upgrade['station']) && $upgrade['station_level'] > 1) 
+                                <em class="block mb-2">Level {{ $upgrade['station_level'] }} {{ $upgrade['station'] }} </em>  
+                            @endif
+                        </li>
+                    @foreach($upgrade['resources'] as $item => $amount)
+                        <li>
+                            @if($amount > 0)
+                                <strong>{{ $amount }}</strong> {{ $item }}
+                            @endif
+                        </li>
+                    @endforeach
+                    </ul>
+                </li>
+            @endforeach
+        </ul>
+        <p class="mt-4">
+            <em>
+                Total resources to max level:<br> 
+                {!! $recipe->totals !!}
+            </em>
+        </p>
+    </div>
+</section>
