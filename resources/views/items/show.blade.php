6519d9538b5da9f05d29f702aa44c767392ba3b6
Jess <code@jessinthe.cloud>
2021-10-21 22:14:14 -0400

    pass $item to partial

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -1,6 +1,7 @@
 <x-layouts.app>
     <x-items.page
         :title="$item->name"
+        :item="$item"
         :partial="'partials._item'"
         :index="'item'"
         :item_class_type="'item'"
